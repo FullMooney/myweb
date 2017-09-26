545f5c2c0382287a517bf4ecf56171d3ed2502a3
cho <tkdlek11112@gmail.com>
2017-09-26 16:29:19 +0900

    no message

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd1rax#9-@i&zf+fesv-#^*r-4_o8fxnl7*^t!lk$roj+v_r(4z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
