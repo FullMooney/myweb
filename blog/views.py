f8db62483ce5234507e6bb8107a0ae72aa71b856
Cho <tkdlek11112@gmail.com>
2017-09-26 16:19:16 +0900

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,42 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
+
+from blog.models import Post
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+#----ListView
+class PostLV(ListView):
+	model=Post
+	template_name = 'blog/post_all.html'
+	context_object_name = 'posts'
+	paginate_by = 2
 
-def post_detail(request,pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html',{'post':post})
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+#----DetailView
+class PostDV(DetailView):
+	model = Post	
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+#-----ArchiveView
+class PostAV(ArchiveIndexView):
+	model = Post
+	date_field = 'modify_date'
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field='modify_date'
+    make_object_list=True
+
+class PostMAV(MonthArchiveView):
+	model = Post
+	date_field = 'modify_date'
+
+class PostDAV(DayArchiveView):
+	model = Post
+	date_field = 'modify_date'
+
+class PostTAV(TodayArchiveView):
+	model = Post
+	date_field = 'modify_date'
