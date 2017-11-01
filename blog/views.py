9453e85b00fa076a7f8206316d2d22105cfcfd52
FullMooney <mooney@hanwha.com>
2017-11-01 18:21:11 +0900

    블로그 리스트 10개씩 보이도록 변경

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ class PostLV(ListView):
 	model=Post
 	template_name = 'blog/post_all.html'
 	context_object_name = 'posts'
-	paginate_by = 2
+	paginate_by = 10
 
 class PostTOL(TaggedObjectList):
 	model = Post
