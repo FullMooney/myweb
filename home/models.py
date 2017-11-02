8010b3b45840d22dc34462004bbb8e7034f7c5aa
Cho <tkdlek11112@gmail.com>
2017-11-02 19:30:29 +0900

    my

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    pirce = 12345
+
+    def get_price(self):
+        return price
---
+++ home/templatetags/stock_filter.py
@@ -0,0 +1,14 @@
+from django import template
+from yahoo_finance import Share
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+register = template.Library()
+
+
+@register.filter(name="get_price")
+def get_price(value):
+    jongmok = Share('YHOO')
+    return value
+#    jongmok = Share('000660.KS')
+#    return "%s"%jongmok.get_price()
