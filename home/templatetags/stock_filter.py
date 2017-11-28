68040bc608607232a18a19b30ebfca16904702f0
cho <tkdlek11112@gmail.com>
2017-11-28 20:34:19 +0900

    commit

--- home/templatetags/stock_filter.py
+++ home/templatetags/stock_filter.py
@@ -1,15 +1,27 @@
 from django import template
-from yahoo_finance import Share
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+import json
+import requests
+
 
 register = template.Library()
 
 
 @register.filter(name="get_price")
 def get_price(value):
-#    jongmok = Share('YHOO')
-    #jongmok = Share('YHOO')
-     return value
+
+     rsp = requests.get('https://finance.google.com/finance?q={}&output=json'.format(value))
+
+     if rsp.status_code in (200,):
+          # This magic here is to cut out various leading characters from the JSON
+          # response, as well as trailing stuff (a terminating ']\n' sequence), and then
+          # we decode the escape sequences in the response
+          # This then allows you to load the resulting string
+          # with the JSON module.
+          fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
+
+          # print out some quote data
+          print('Opening Price: {}'.format(fin_data['l']))
+
+     return fin_data['l']
 #   jongmok = Share('000660.KS')
 #   return "%s"%jongmok.get_price()
