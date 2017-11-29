92299c99d9d42bde9d0242d3ea41b892ab8b13df
Kim Raehyun <kimrh@users.noreply.github.com>
2017-11-30 00:20:14 +0900

    install pyowm

--- home/templatetags/stock_filter.py
+++ home/templatetags/stock_filter.py
@@ -1,5 +1,7 @@
 from django import template
 from yahoo_finance import Share
+from pyowm import OWM
+
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -13,3 +15,19 @@ def get_price(value):
      return value
 #   jongmok = Share('000660.KS')
 #   return "%s"%jongmok.get_price()
+
+
+@register.filter(name="get_weather")
+def get_weather(value):
+#   return "test"
+    print('weather')
+    WAETHERAPI_key = '54bfc8086ddec1998083663d49c57c9a'
+    owm = OWM(WAETHERAPI_key)
+    obs = owm.weather_at_place('Seoul')
+    w = obs.get_weather()
+    resultVal ='seoul : ' + w.get_status() + ', '+ str(w.get_temperature(unit='celsius')['temp'])
+    print('Seoul : ', w.get_status(), w.get_temperature(unit='celsius')['temp'])
+    return resultVal
+
+
+
