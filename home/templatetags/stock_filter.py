63cc9f208e6c6988e59412e86f91c634530f3c76
Kim Raehyun <kimrh@users.noreply.github.com>
2017-11-30 11:48:39 +0900

    미세먼지 농도 추가

--- home/templatetags/stock_filter.py
+++ home/templatetags/stock_filter.py
@@ -1,7 +1,11 @@
 from django import template
 from yahoo_finance import Share
 from pyowm import OWM
-
+import feedparser
+import urllib
+import xmltodict
+from urllib.parse   import quote
+from urllib.request import urlopen
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -31,3 +35,34 @@ def get_weather(value):
 
 
 
+@register.filter(name="get_airdata")
+def get_airdata(value):
+
+    url_1 = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName="
+    datalocation = urllib.parse.quote_plus("종로구")
+    url_2 = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=kvpslAYfknZmVasn9HiQQbE2iKy%2FxU%2BS8tRQWI4YjE%2BygdoLDgYpxEDJHmIU1lS2Mwp7WxIF%2FD2E208WkTecQA%3D%3D&ver=1.3"
+
+    url = url_1 + datalocation + url_2
+    # req = urllib2.Request(url)
+    req = urllib.request.urlopen(url)
+    #print(url)
+    data = req.read()
+
+    xmldata = xmltodict.parse(data)
+    # xmldata = feedparser.parse(data)
+    xmldata = xmldata["response"]["body"]["items"]
+    # xmldata = xmldata["body"]
+    #print(xmldata)
+
+    context = {}
+    context['xmldata'] = xmldata
+
+    airDataList = xmldata['item']
+    #print(airDataList)
+
+    resultAirData = airDataList[1]['pm25Value']
+    print(resultAirData)
+
+    #print('------- '+resultAirData)
+
+    return resultAirData
